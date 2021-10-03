02d9efedbe5e18d2fa1a8d0420c74e701ab7664b
Mark Nasol <marknasol95@gmail.com>
2021-10-03 00:39:03 -0400

    Completed implementation of Orders class - Added destructor to Orders class - Modified private members to be pointer types - Started implementing the OrdersList class - Added constructors and destructors to the Orders' subclasses (Deploy, Advance, Bomb, Blockade, Airlift, Negotiate)

--- Orders.h
+++ Orders.h
@@ -4,71 +4,77 @@
 #include <string>
 using namespace std;
 
-class Orders{
-    public:
-        Orders();
-        Orders(string typeValue, string descValue, string effectValue);
-        bool validate();
-        void execute();
-        string getType();
-        string getDescription();
-        string getEffect();
-        void setType(string newType);
-        void setDescription(string newDescription);
-        void setEffect(string newEffect);
-    private:
-        string type;
-        string description;
-        string effect;
+class Orders
+{
+public:
+    Orders();
+    ~Orders();
+
+    Orders(string typeValue, string descValue, string effectValue);
+    bool validate();
+    void execute();
+    string getType();
+    string getDescription();
+    string getEffect();
+    void setType(string newType);
+    void setDescription(string newDescription);
+    void setEffect(string newEffect);
+
+private:
+    string * type;
+    string * description;
+    string * effect;
 };
 
-
-class OrdersList{
-
+class OrdersList
+{
+public:
+    OrdersList();
+    ~OrdersList();
+    void move();
+    void remove();
 };
 
-class Deploy: public Orders{
-
+class Deploy : public Orders
+{
+public:
+    Deploy();
+    ~Deploy();
 };
 
-Deploy::Deploy() : Orders("deploy", "place some armies on one of the current player’s territories.", "")
-{}
-
-class Advance: public Orders{
-
+class Advance : public Orders
+{
+public:
+    Advance();
+    ~Advance();
 };
 
-Advance::Advance() : Orders("advance",
- "move some armies from one of the current player’s territories (source) to an adjacent territory (target). If the target territory belongs to the current player, the armies are moved to the target territory. If the target territory belongs to another player, an attack happens between the two territories",
-  "")
-{}
-
-class Bomb: public Orders{
-
+class Bomb : public Orders
+{
+public:
+    Bomb();
+    ~Bomb();
 };
 
-Bomb::Bomb() : Orders("bomb", "destroy half of the armies located on an opponent’s territory that is adjacent to one of the current player’s territories", "")
-{}
-
-class Blockade: public Orders{
-
+class Blockade : public Orders
+{
+public:
+    Blockade();
+    ~Blockade();
 };
 
-Blockade::Blockade() : Orders("blockade", "triple the number of armies on one of the current player’s territories and make it a neutral territory", "")
-{}
-
-class Airlift: public Orders{
-
+class Airlift : public Orders
+{
+public:
+    Airlift();
+    ~Airlift();
 };
 
-Airlift::Airlift() : Orders("airlift", "advance some armies from one of the current player’s territories to any another territory", "")
-{}
-
-class Negotiate: public Orders{
-
+class Negotiate : public Orders
+{
+public:
+    Negotiate();
+    ~Negotiate();
 };
-
-Negotiate::Negotiate() : Orders("negotiate", "prevent attacks between the current player and another player until the end of the turn", "")
-{}
 
 #endif
