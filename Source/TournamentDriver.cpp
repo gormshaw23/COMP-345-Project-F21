9b915e04bdc0ad9ca06719d730312708ff6f4e9f
Mark Nasol <marknasol95@gmail.com>
2021-12-03 16:47:02 -0500

    Added test statements for the TournamentDriver - Not yet tested due to missing implementation

--- Source/TournamentDriver.cpp
+++ Source/TournamentDriver.cpp
@@ -8,36 +8,39 @@
  * @author
  */
 
-
 #include "Engine/GameEngine.h"
+#include "Strategy/PlayerStrategies.h"
 #include <Map/map.h>
 
 
 int main() {
 	/*
 	-----------------------------------------------------------------
-		Test code for checking if the game has exceeded the maximum \
-		number of turns
+		Test code for checking if the game has exceeded the maximum 
+		number of turns and the execution of tournament mode
 	-----------------------------------------------------------------
 	*/
-	////Testing the main game loop with a number of maximum turns
-	//int maxTurns = 5;
+	//Testing the main game loop with a number of maximum turns
+	int maxTurns = 5;
 
-	////Load map
-	//MapLoader* mapL = new MapLoader();
-	//mapL->loadMap("canada");
-	//Map* map = mapL->getListMap()->at(0);
+	//Load map
+	MapLoader* mapL = new MapLoader();
+	mapL->loadMap("canada");
+	Map* map = mapL->getListMap()->at(0);
 
-	////Create game engine
-	//GameEngine* ge = new GameEngine();
+	//Create game engine
+	GameEngine* ge = new GameEngine();
+	ge->setIsATournament(true);
 
-	////Create players
-	//Player* p1 = new Player("player1");
-	//Player* p2 = new Player("player2");
-	//p1->setReinforcementPool(50);
-	//p2->setReinforcementPool(50);
-	//std::vector<Player*> players = { p1, p2 };
+	//Create players
+	Player* p1 = new Player("player1");
+	Player* p2 = new Player("player2");
+	p1->setPlayerStrategy(new AggressivePlayerStrategy());
+	p1->setReinforcementPool(50);
+	p2->setPlayerStrategy(new BenevolentPlayerStrategy());
+	p2->setReinforcementPool(50);
+	std::vector<Player*> players = { p1, p2 };
 
-	//ge->mainGameLoop(players, map, 5);
+	ge->mainGameLoop(players, map, 5);
 	return 0;
 }
